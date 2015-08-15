d481ea27fa060ba90067828332f14b4b622b3f60
Steffen Baer <stbaer.mail@gmail.com>
2015-08-15 19:35:50 +0800

    bumped version; updated build; added untracked files

---
+++ src/const.js
@@ -0,0 +1,15 @@
+var CONST = {};
+
+CONST.MAX_SET_BY_DEFAULT = 100;
+CONST.HANDLE_RESIZE_DEBOUNCE = 100;
+CONST.RANGE_CLASS = 'rangeslider';
+CONST.FILL_CLASS = 'rangeslider__fill';
+CONST.FILL_BG_CLASS = 'rangeslider__fill__bg';
+CONST.HANDLE_CLASS = 'rangeslider__handle';
+CONST.DISABLED_CLASS = 'rangeslider--disabled';
+CONST.STEP_SET_BY_DEFAULT = 1;
+CONST.START_EVENTS = ['mousedown', 'touchstart', 'pointerdown'];
+CONST.MOVE_EVENTS = ['mousemove', 'touchmove', 'pointermove'];
+CONST.END_EVENTS = ['mouseup', 'touchend', 'pointerup'];
+
+module.exports = CONST;
